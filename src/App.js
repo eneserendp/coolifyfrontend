d69ac9fec02de6e2a69a73ef1fdd6340a46b6492
Enes Eren Demirpolat <enes_erendemirpolat@hotmail.com>
2025-05-30 17:35:22 +0300

    Update nginx config and API URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+// API URL'ini doğrudan belirtelim
+const API_URL = 'http://193.111.125.236:3002';
 
 function App() {
   const [users, setUsers] = useState([]);
